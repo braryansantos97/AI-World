c3b6a951ca5b9d06f574923b550366f40caaeaf0
Brayan Santos <santos.a.brayan97@gmail.com>
2022-05-18 23:53:11 -0400

    app for deployment

--- server.js
+++ server.js
@@ -1,30 +1,54 @@
 require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+// const express = require('express');
+// const axios = require('axios');
+// const cors = require('cors');
+// const app = express();
+// const PORT = process.env.PORT || 8000;
+// // const mongoose = require('mongoose');
+// const MONGODB_URI = process.env.MONGODB_URI;
+// const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+// // const db = mongoose.connection;
+
+
+// // mongoose.connect(MONGODB_URI, {
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// // });
+// // db.on('open', () => {
+// //     console.log('Mongo is Connected');
+// // });
+
+// app.use(express.json());
+// app.use(cors());
+
+// // app.get('/chatai', (req,res) => {
+
+// // })
+
+// app.listen(PORT,()=>{
+//     console.log('Server Connected', PORT);
+// });
+
+
+const path = require('path');
+const express = require("express");
 const app = express();
+const {createServer} = require('http');
+
+app.use(express.static('build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/build/index.html`))
+})
+
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 8000;
-// const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
-const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
-// const db = mongoose.connection;
+// const {Server} = require("socket.io");
+// const io = new Server(httpServer);
 
-
-// mongoose.connect(MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
+// io.on('connection', function(socket) {
+//   socket.on('send', function(message){
+//       io.emit('receive', message);
+//   });
 // });
-// db.on('open', () => {
-//     console.log('Mongo is Connected');
-// });
-
-app.use(express.json());
-app.use(cors());
-
-// app.get('/chatai', (req,res) => {
-
-// })
-
-app.listen(PORT,()=>{
-    console.log('Server Connected', PORT);
-});
+httpServer.listen(PORT);
